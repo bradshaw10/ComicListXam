50238332e543cae4a5cc96afd3c848fba0a44894
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-04-20 21:28:43 +0400

    inirial commit

---
+++ ComicListXam/ComicListXam/ComicListXam/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ComicListXam.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ComicListXam.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
